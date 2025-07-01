fcb915053de58e8c36fe77e7b5f47ca29f1b9371
samerspc <mas.norvg13@gmail.com>
2025-07-01 22:53:49 +0300

    feat: update frontend dependencies

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@shared': resolve(__dirname, 'src/shared'),
+      
+    }
+  }
 })
